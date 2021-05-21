ec40eabb4ed113a57be49f32c58bbb6ea064fbe7
Tarun Gunampalli <tarun.gunampalli@gmail.com>
2021-05-21 14:17:17 -0500

    added places api call made new schema

--- app.js
+++ app.js
@@ -1,86 +1,82 @@
-// var express = require("express");
-// var app = express();
-// var port = 3000;
-
-// app.use("/", (req, res) => {
-//     res.sendFile(__dirname + "/index.html");
-// });
-
-// // app.get("/", (req, res) => {
-// //     res.send("Hello World");
-// // });
-
-// var mongoose = require("mongoose");
-// mongoose.Promise = global.Promise;
-// mongoose.connect("mongodb://localhost:27017/node-demo", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
-
-// var nameSchema = new mongoose.Schema({
-//     firstName: String,
-//     lastNameName: String,
-// });
-
-// var User = mongoose.model("User", nameSchema);
-
-// app.post("/addname", (req, res) => {
-//     var myData = new User(req.body);
-//     myData
-//         .save()
-//         .then((item) => {
-//             res.send("item saved to database");
-//         })
-//         .catch((err) => {
-//             res.status(400).send("unable to save to database");
-//         });
-// });
-
-// var bodyParser = require("body-parser");
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.listen(port, () => {
-//     console.log("Server listening on port " + port);
-// });
-
+// set up express
 var express = require("express");
 var app = express();
 var port = 3000;
-var bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
+// uses .env file for google api key
+require("dotenv").config();
+
+// set up parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// import for requests
+const fetch = require("node-fetch");
+
+// set up mongoose
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/hitch-test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-var nameSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-});
-var User = mongoose.model("Person", nameSchema);
 
+// scheme for places
+var placeSchema = new mongoose.Schema({
+    address: String,
+    state: String,
+    zipCode: String, // ? Maybe String
+});
+var Place = mongoose.model("Place", placeSchema);
+
+// use index.html for frontend
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/addname", (req, res) => {
-    // console.log(req.body);
-    var myData = new User(req.body);
-    console.log(myData);
-    myData.save(function (err) {
+app.post("/getPlaceInfo", async (req, res) => {
+    let placeInfo = await getPlaceInfo(req.body.originPlace_id);
+    if (!placeInfo) {
+        res.send("Place details not found");
+        return;
+    }
+    // res.send("Place details found");
+    data = { address: placeInfo?.formatted_address };
+    placeInfo?.address_components.map((component) => {
+        console.log(component);
+        if (component.types.includes("postal_code")) {
+            data["zipCode"] = component.long_name;
+        }
+        if (component.types.includes("administrative_area_level_1")) {
+            data["state"] = component.long_name;
+        }
+    });
+
+    const newEntry = new Place(data);
+    newEntry.save(function (err) {
         if (err) {
             console.log(err);
             res.status(400).send("Unable to save to database");
             return;
         }
 
-        res.send("Name saved to database");
+        res.send("Place saved to database");
     });
 });
+
+// make request to google maps api
+async function getPlaceInfo(placeId) {
+    let url =
+        "https://maps.googleapis.com/maps/api/place/details/json?" +
+        new URLSearchParams({
+            place_id: placeId,
+            fields: ["address_components", "formatted_address"],
+            key: process.env.API_KEY,
+        });
+    let response = await fetch(url);
+    response = await response.json();
+    return response.result;
+}
 
 app.listen(port, () => {
     console.log("Server listening on port " + port);
